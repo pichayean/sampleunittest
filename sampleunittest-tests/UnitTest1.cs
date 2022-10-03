f34d322feb4c23b1af7d4dfefc1b955e1f6639d3
pichayean <pichayeanyensiri@gmail.com>
2022-10-03 20:34:36 +0700

    Test Case

--- sampleunittest-tests/UnitTest1.cs
+++ sampleunittest-tests/UnitTest1.cs
@@ -5,6 +5,36 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test2()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test4()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test5()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test6()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test7()
+    {
+        Assert.True(true);
     }
 }
